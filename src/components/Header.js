0f8fa448c4940ed1ca46306a261d7b5b582981f0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 20:45:34 +0600

    dfsd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <div className="navbar bg-neutral text-neutral-content">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
-      <div className="navbar bg-base-300">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
       <div className="navbar bg-primary text-primary-content">
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Link to="home">Home</Link>
+        <Link to="login">Log in</Link>
+        <Link to="register">Register</Link>
       </div>
     </div>
   );
