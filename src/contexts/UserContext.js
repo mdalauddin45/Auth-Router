b0c0d88cd1ee29f97ccf4171ef6e2d70a14ac2e4
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 22:33:40 +0600

    fdgf

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,9 +1,14 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
+
 const UserContext = ({ children }) => {
-  const user = { displayName: "Alauddin" };
+  const [user, setUser] = useState({ displayName: "Alauddin" });
+
   const authInfo = { user: user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
