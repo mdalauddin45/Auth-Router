1c61dcb13365c161ab6a2bba5328ef18b28b1fe6
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 20:48:01 +0600

    sgff

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,19 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <div className="navbar bg-primary text-primary-content">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        <Link to="home">Home</Link>
-        <Link to="login">Log in</Link>
-        <Link to="register">Register</Link>
+      <div className="navbar bg-primary text-primary-content ">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          SufiBrother
+        </Link>
+        <Link to="home" className="btn btn-ghost normal-case text-xl">
+          Home
+        </Link>
+        <Link to="login" className="btn btn-ghost normal-case text-xl">
+          Log in
+        </Link>
+        <Link to="register" className="btn btn-ghost normal-case text-xl">
+          Register
+        </Link>
       </div>
     </div>
   );
