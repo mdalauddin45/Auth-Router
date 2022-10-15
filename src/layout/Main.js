8702e871b696377ead872bca17646c4ae737db29
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 20:38:54 +0600

    sagdf

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 function Main() {
   return (
     <div>
-      <h1>hello</h1>
+      <Header></Header>
+      <Outlet></Outlet>
     </div>
   );
 }
