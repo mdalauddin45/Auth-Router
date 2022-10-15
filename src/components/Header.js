d63d01edba071089caf4cf115f1274ca61893d2e
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 20:43:40 +0600

    sadfsdg

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>this is header</h1>
+      <div className="navbar bg-neutral text-neutral-content">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar bg-base-300">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar bg-primary text-primary-content">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
     </div>
   );
 };
