3c7f114a7d874822c2e36ab65f9104c876dac85a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 22:27:42 +0600

    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../contexts/UserContext";
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
+  console.log("context", user);
   return (
     <div>
-      <h1>This is home</h1>
+      <h1>This is home for {user?.displayName}</h1>
     </div>
   );
 };
